828408c57c2bc4e4517987b534e9019dc3ceeffe
erakshaykumar <akaakshay304@gmail.com>
2022-03-04 01:28:59 +0530

    Modified: Binary Search Word From Array Logic

--- src/com/blz/datastructure/BinarySearchWord.java
+++ src/com/blz/datastructure/BinarySearchWord.java
@@ -1,9 +1,19 @@
+/*@Purpose:Read in a list of words from File. Then prompt the user to enter a word to
+ * search the list. The program reports if the search word is found in the list.
+ * @File: Binary Search the Word from Word List
+ * @Author: Akshay Kumar
+ */
 package com.blz.datastructure;
 
 import java.util.Arrays;
 
 public class BinarySearchWord {
 
+	/*
+	 * @Purpose:Binary Search the Word from Word List
+	 * 
+	 * @Param: Akshay,Karthik,Rushi,Shubham,Suraj,Susobhsan
+	 */
 	public static void main(String[] args) {
 		String[] arrayElements = { "Akshay", "Karthik", "Rushi", "Shubham", "Suraj", "Susobhan" };
 		Arrays.sort(arrayElements);
@@ -20,14 +30,14 @@ public class BinarySearchWord {
 	static int binarySearch(String[] arrayElements, String search) {
 		int left = 0, right = arrayElements.length - 1;
 		while (left <= right) {
-			int m = left + (right - left) / 2;
-			int result = search.compareTo(arrayElements[m]);
+			int member = left + (right - left) / 2;
+			int result = search.compareTo(arrayElements[member]);
 			if (result == 0)
-				return m;
+				return member;
 			if (result > 0)
-				left = m + 1;
+				left = member + 1;
 			else
-				right = m - 1;
+				right = member - 1;
 		}
 
 		return -1;
